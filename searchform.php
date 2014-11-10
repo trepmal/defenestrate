e4b8f621a126023e7736c9ce5b3109decc53a682
Kailey Lampert <trepmal@gmail.com>
2014-11-09 20:17:09 -0800

    remove search label

--- searchform.php
+++ searchform.php
@@ -1,4 +1,3 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<span class="genericon genericon-search"></span>
-	<input type="text" value="<?php echo get_search_query(); ?>" name="s" class="searchbar" />
+	<input type="text" value="<?php echo get_search_query(); ?>" name="s" class="searchbar" title="Search" />
 </form>
